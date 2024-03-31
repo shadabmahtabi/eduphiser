6fdb64ff270d69cd63b28cfaf8442a16593d1880
Shadab Mahtabi <shadabmanpuri@gmail.com>
2024-03-31 16:28:46 +0530

    handling asynchronous errors

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,9 @@
-exports.homepage = (req, res, next) => {
+const { catchAsynchErrors } = require("../middlewares/catchAsynchErrors")
+
+exports.homepage = catchAsynchErrors( async (req, res, next) => {
     res.json({
         "name" : "Shadab",
         "age" : 20,
         "branch" : "CSE"
     })
-}
+})
